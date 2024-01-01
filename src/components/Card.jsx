a9cb026a6390d0fb366e4e123d98a136cb32bb12
PN-G <139358746+PN-G@users.noreply.github.com>
2024-01-01 21:23:28 +0100

    Ajout du fichier json + génération dynamique des cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 
-const Card = () => {
+const Card = ({ logement }) => {
     return (
         <div className="card">
-            <h2 className="card__title">Titre de la location</h2>            
+            <h2 className="card__title">{logement.title}</h2>            
         </div>
     );
 };
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,12 @@
 import Card from "./Card";
+import data from "../data/logements.json";
 
 const CardContainer = () => {
     return (
         <div className="card-container">
-            <Card />
-            <Card />
-            <Card />            
+            {data.map((logement, index) => (
+                <Card key={index} logement={logement} />
+                ))}
         </div>
     );
 };
